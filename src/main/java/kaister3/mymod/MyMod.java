286320d5f899a41b01c74b57e2f8bc8f7ab0dc67
kaister3 <kaister3@protonmail.com>
2021-03-11 17:02:05 +0800

    hello world

---
+++ src/main/java/kaister3/mymod/MyMod.java
@@ -0,0 +1,10 @@
+package kaister3.mymod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MyMod {
+   private static final Logger LOGGER= LogManager.getLogger();
+
+   public static final
+}
